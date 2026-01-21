4ec9fb8f9bf5f11b74a3c616ccbbe1291896256e
julien Michelin <j.michelin@fougeres.fr>
2026-01-21 13:54:34 +0100

    Test d'ajout d'une nouvelle page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
+    #[Route('/index', name: 'index', methods: [GET])]
     public function index(): Response
     {
         return $this->render('default/index.html.twig');
+    }
+
+    #[Route('/info', name: 'info', methods: [GET])]
+    public function info(): Response
+    {
+        return $this->render('default/info.html.twig', [
+            "info" => "ahahahaha",
+        ]);
     }
 }
