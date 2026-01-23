9e3609d9d33416d7ec71c578f88f6dc082267732
SOK Ewen <e.sok@hdv.local>
2026-01-23 14:33:59 +0100

    Correction du build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const Encore = require('@symfony/webpack-encore');
 
 Encore
   .setOutputPath('public/build/')
-  .setPublicPath('/truc/qcm/public/build') // Dans le public path, il faut mettre le lien qui mène vers le build et pas juste /build
+  .setPublicPath('/stages_fougeres_owen/qcm/build') // Dans le public path, il faut mettre le lien qui mène vers le build et pas juste /build
+  .setManifestKeyPrefix('build/')
   .enableStimulusBridge('./assets/controllers.json') // ← important
   .addEntry('app', './assets/app.js')
   .enableSassLoader()
