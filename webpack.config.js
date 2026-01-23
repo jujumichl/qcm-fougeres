252a7fd660554353d4efcf6e5db124c36f7af1d7
MICHELIN Julien <j.michelin@hdv.local>
2026-01-23 14:44:18 +0100

    Correction du webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const Encore = require('@symfony/webpack-encore');
 
 Encore
   .setOutputPath('public/build/')
-  .setPublicPath('/stages_fougeres_owen/qcm/build') // Dans le public path, il faut mettre le lien qui mène vers le build et pas juste /build
-  .setManifestKeyPrefix('build/')
+  .setPublicPath('/stage_fougeres_owen/qcm/public/build') // Dans le public path, il faut mettre le lien qui mène vers le build et pas juste /build
   .enableStimulusBridge('./assets/controllers.json') // ← important
   .addEntry('app', './assets/app.js')
   .enableSassLoader()
