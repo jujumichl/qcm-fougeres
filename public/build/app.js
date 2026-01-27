366ee9372a88142ed0157cc551fae2a967bbd1b9
MICHELIN Julien <j.michelin@hdv.local>
2026-01-27 14:05:32 +0100

    Mise en place de la normalisation

---
+++ public/build/app.js
@@ -0,0 +1,57 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["app"],{
+
+/***/ "./assets/app.js"
+/*!***********************!*\
+  !*** ./assets/app.js ***!
+  \***********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _stimulus_bootstrap_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./stimulus_bootstrap.js */ "./assets/stimulus_bootstrap.js");
+/* harmony import */ var _styles_app_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/app.css */ "./assets/styles/app.css");
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+/***/ },
+
+/***/ "./assets/stimulus_bootstrap.js"
+/*!**************************************!*\
+  !*** ./assets/stimulus_bootstrap.js ***!
+  \**************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '@symfony/stimulus-bundle'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+var app = Object(function webpackMissingModule() { var e = new Error("Cannot find module '@symfony/stimulus-bundle'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
+
+/***/ },
+
+/***/ "./assets/styles/app.css"
+/*!*******************************!*\
+  !*** ./assets/styles/app.css ***!
+  \*******************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/app.js"));
+/******/ }
+]);
