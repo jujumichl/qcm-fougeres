ea584792bc75a9343140ad49a0f1c4c2ca4e45c9
SOK Ewen <e.sok@hdv.local>
2026-02-02 14:58:16 +0100

    #6 Ajout sur la génération de réponse (input, style)

--- assets/controllers/edition_controller.js
+++ assets/controllers/edition_controller.js
@@ -3,73 +3,64 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="login"
 export default class extends Controller {
-  static targets = ["reponse", "divReponse", "addRepBtn"]
-
-  // Vérifie que le controller est bien connecté et fontionnelle
-  connect() {
-    console.log("🔥 STIMULUS FONCTIONNE 🔥");
-  }
+  static targets = ["reponse", "bodyReponse"]
 
   ajoutReponse() {
-    // Vérifie si une target qcmReponse existe dans le html
-    if (!this.hasDivReponseTarget) {
-      console.log("Pas de conteneur de réponses trouvé !");
-      return;
-    }
 
     // On compte combien de réponses déjà existantes
     let num = this.reponseTargets.length + 1
 
-    if(num - 1 > 0){  
+    if (num - 1 > 0) {
       let targetNum = this.reponseTargets[num - 2].id;
-      targetNum = targetNum.split("p") 
-      num = Number(targetNum[1]) + 1
+      targetNum = targetNum.split("p");
+      num = Number(targetNum[1]) + 1;
       console.log(num);
     }
 
-    if (this.reponseTargets.length < 5) {
-      
-      // Crée le nouvel élément
-      const div = document.createElement("div");
-      div.className = "d-flex align-items-center gap-2";
+    // Crée le nouvel élément
+    const div = document.createElement("div");
+    div.className = "d-flex align-items-center gap-3";
 
-      const btnSuppr = document.createElement("button");
-      btnSuppr.type = "button";
-      btnSuppr.className = "btn";
-      btnSuppr.title = "supprimer";
-      btnSuppr.dataset.action = "click->edition#supprReponse";
+    const btnSuppr = document.createElement("button");
+    btnSuppr.type = "button";
+    btnSuppr.className = "btn mb-3";
+    btnSuppr.title = "supprimer";
+    btnSuppr.dataset.action = "click->edition#supprReponse";
 
-      // Création de l’icône
-      const icon = document.createElement("i");
-      icon.className = "bi bi-dash-circle";
-      icon.style.color = "red";
+    // Création de l’icône
+    const icon = document.createElement("i");
+    icon.className = "bi bi-dash-circle";
+    icon.style.color = "red";
 
-      // Ajout de l’icône dans le bouton
-      btnSuppr.appendChild(icon);
+    // Ajout de l’icône dans le bouton
+    btnSuppr.appendChild(icon);
 
-      // Crée le nouvel input et label
-      const input = document.createElement("input");
-      input.type = "checkbox";
-      input.name = "reponse"; // même groupe
-      input.id = `rep${num}`;
-      input.value = `Option ${num}`;
-      input.className = "form-check-input";
-      input.dataset.editionTarget = "reponse";
+    // Crée le nouvel input qui sera visible côté utilisateur
+    const inputUser = document.createElement("input");
+    inputUser.type = "checkbox";
+    inputUser.name = "userRep"; // même groupe pour tous les inputs user
+    inputUser.id = `repUser${num}`;
+    inputUser.className = "form-check-input mb-3";
+    inputUser.dataset.editionTarget = "reponse";
+    inputUser.disabled = "true";
 
-      const label = document.createElement("label");
-      label.htmlFor = input.id;
-      label.textContent = `Option ${num}`;
-      label.className = "form-check-label";
-
-      div.appendChild(btnSuppr);
-      div.appendChild(input);
-      div.appendChild(label);
-
-      const btnRep = this.addRepBtnTarget;
-
-      this.divReponseTarget.insertBefore(div, btnRep);
+    // Crée le nouvel input pour insérer une réponse côté edition
+    const inputRep = document.createElement("input");
+    inputRep.type = "text";
+    inputRep.name = "editRep";
+    inputRep.id = `rep${num}`;
+    inputRep.className = "form-control mb-3";
+    if (num === 1) {
+      inputRep.placeholder = "Mettez une réponse...";
     }
+
+    div.append(btnSuppr);
+    div.append(inputUser);
+    div.append(inputRep);
+
+    this.bodyReponseTarget.append(div);
   }
+
   supprReponse(event) {
     // bouton cliqué
     const button = event.currentTarget;
@@ -79,4 +70,5 @@ export default class extends Controller {
 
     reponseDiv.remove();
   }
+
 }
